26a18b9a228fb1aded329697412589500640bfcc
BHAVANA <bhavanaroyalp@gmail.com>
2023-04-18 22:29:07 +0530

    first commit

---
+++ Dindiproject.java
@@ -0,0 +1,11 @@
+
+public class Dindiproject {
+    public static void main(String[] args) {
+		 int y=88;
+		 int b=55;
+		 int w=890;
+		 
+		 int grp=y+b*w;
+		 System.out.println(grp);
+    }
+}
---
+++ Tatamotots.java
@@ -0,0 +1,11 @@
+
+public class Tatamotots {
+         public static void main(String[] args) {
+			int p=32;
+			int j=99999;
+			  int f=76;
+			  int pt=p*j+f;
+			  
+			 System.out.println(pt);
+		}
+}
