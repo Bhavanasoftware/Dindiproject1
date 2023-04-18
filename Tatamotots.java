9773e93b33873b06c353c7c2a2dc0711305acec4
BHAVANA <bhavanaroyalp@gmail.com>
2023-04-18 22:49:33 +0530

    changes made in Tatamotots.java

--- Tatamotots.java
+++ Tatamotots.java
@@ -3,7 +3,7 @@ public class Tatamotots {
          public static void main(String[] args) {
 			int p=32;
 			int j=99999;
-			  int f=76;
+			  int f=762;
 			  int pt=p*j+f;
 			  
 			 System.out.println(pt);
